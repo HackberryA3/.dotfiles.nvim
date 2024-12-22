b6a1079738c00a560b433ea5e57794c3f7cf131c
HackberryA3 <1107a3@gmail.com>
2024-12-22 01:30:57 +0000

    Added avante that AI integration plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,6 +13,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 -- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+vim.g.mapleader = " " -- Lazyよりも前に設定する
+vim.g.maplocalleader = "\\"
+
 require("lazy").setup({
 		-- ヘルプを日本語にする
 		"vim-jp/vimdoc-ja",
@@ -98,6 +101,50 @@ require("lazy").setup({
 		},
 		-- ステータスラインに状態を表示
 		{ "AndreM222/copilot-lualine" },
+		{
+			"yetone/avante.nvim",
+			event = "VeryLazy",
+			lazy = false,
+			version = false, -- set this if you want to always pull the latest change
+			opts = require("configs.avante"),
+			-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+			build = "make",
+			-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+			dependencies = {
+				"stevearc/dressing.nvim",
+				"nvim-lua/plenary.nvim",
+				"MunifTanjim/nui.nvim",
+				--- The below dependencies are optional,
+				"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+				"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+				"zbirenbaum/copilot.lua", -- for providers='copilot'
+				{
+					-- support for image pasting
+					"HakonHarnes/img-clip.nvim",
+					event = "VeryLazy",
+					opts = {
+						-- recommended settings
+						default = {
+							embed_image_as_base64 = false,
+							prompt_for_file_name = false,
+							drag_and_drop = {
+								insert_mode = true,
+							},
+							-- required for Windows users
+							use_absolute_path = true,
+						},
+					},
+				},
+				{
+					-- Make sure to set this up properly if you have lazy=true
+					'MeanderingProgrammer/render-markdown.nvim',
+					opts = {
+						file_types = { "markdown", "Avante" },
+					},
+					ft = { "markdown", "Avante" },
+				},
+			},
+		},
 
 
 
